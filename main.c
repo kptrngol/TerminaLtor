c3796385ce4c2723471e5fbc1fcf69d278831d28
Konrad Petrenko-Goljanek <k.petrenkogoljanek.068@studms.ug.edu.pl>
2023-12-12 17:57:03 +0100

    wall collision detection

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void drawScene(int mapH, int mapW, char (*p)[21]);
+
+void drawScene();
 void drawAvatar();
 void playerInput();
 void clear();
 void check();
+void savingPreviousAvatarPos();
+int collisionDetection();
+
 
 int main() {
+
     int mapH, mapW, avatarR, avatarC, avatarLastC, avatarLastR, win;
     win = 0;
     mapH = 5;
@@ -17,32 +22,33 @@ int main() {
     avatarLastR = 3;
     avatarLastC = 1;
     int * pWin = &win;
-    int * pAvatarR = &avatarR;
-    int * pAvatarC = &avatarC;
-    int * pAvatarLastR = &avatarLastR;
-    int * pAvatarLastC = &avatarLastC;
-    
+
 
     char gameMap[5][21] = 
     {
         {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','\n'},
-        {'#','&','#','#',' ','#',' ',' ',' ',' ','#',' ','#','#',' ','#',' ','#',' ','$','\n'},
+        {'#','$','#','#','$','#',' ',' ',' ',' ','#','$','#','#',' ','#',' ','#',' ','@','\n'},
         {'#',' ',' ','#',' ',' ',' ',' ','#','#','#',' ','#','#',' ','#',' ',' ',' ','#','\n'},
         {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ','#','\n'},
         {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','\n'}
     };
 
     char (*ptoMap)[21] = gameMap;
-
+    printf("\n!Type a/w/s/d and click enter to move!\n\n");
+    
     while(win != 1)
     {
     drawAvatar(avatarR,avatarC,ptoMap);
     drawScene(mapH, mapW, ptoMap);
     check(&pWin, ptoMap);
+    savingPreviousAvatarPos(&avatarLastR,&avatarLastC,&avatarR,&avatarC);
     clear(avatarR, avatarC, ptoMap);
-    playerInput(&avatarR,&avatarC); 
+    // playerInput(&avatarR,&avatarC);
+    do 
+    {
+        playerInput(&avatarR,&avatarC);
+    } while (collisionDetection(ptoMap,&avatarLastR,&avatarLastC,&avatarR,&avatarC));
     }
-
 
 }
 
@@ -57,17 +63,19 @@ void drawScene(int mapH, int mapW, char (*p)[21])
     }
     printf("\n");
 }
-
 void drawAvatar(int r,int c, char (*p)[21]) 
 {
 
-    p[r][c] = 'A';
+    p[r][c] = 'H';
 }
-
 void playerInput(int *row,int *column) 
 {
     char mv;
+    do 
+    {
     scanf("%c",&mv);
+    } while ((mv != 'a') && (mv != 'd') && (mv != 'w') && (mv != 's'));
+
     switch(mv)
     {
         case 'a':
@@ -85,7 +93,6 @@ void playerInput(int *row,int *column)
     }
     
 }
-
 void clear(int r, int c, char (*p)[21]) 
 {
     p[r][c] = ' ';
@@ -93,8 +100,32 @@ void clear(int r, int c, char (*p)[21])
 void check(int*w,char (*p)[21])
 {
     printf("Absorb the %c!\n",p[1][19]);
-    if ((p[1][19]) == 'A') 
+    if ((p[1][19]) == 'H') 
     {
         exit(EXIT_SUCCESS);
     }
+}
+void savingPreviousAvatarPos(int * lastR,int * lastC,int * r, int * c)
+{
+    *lastR = *r;
+    *lastC = *c;
+
+}
+int collisionDetection(char (*p)[21],int * lastR,int * lastC,int * r,int * c)
+{
+    int r1, c1;
+    char el;
+    r1 = *r;
+    c1 = *c;
+    el = p[r1][c1];
+    if (el == '#') 
+    {   
+        *r = *lastR;
+        *c = *lastC;
+        return 1;
+    } else 
+    {
+        return 0;
+    }
+    return 0;
 }
